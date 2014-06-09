96444922174443bcb542edb5d5725e91a458c7db
norahiko <norahiko.o@gmail.com>
2014-06-09 23:55:34 +0900

    Rename Watch -> Watcher

--- test/helper_test.js
+++ test/helper_test.js
@@ -9,7 +9,7 @@ var deepEqual = assert.deepEqual;
 suite('Lazylib:', function() {
     test('lazylib', function() {
         assert.isDefined(helper.lazylib.glob, 'lazylib.glob');
-        assert.isDefined(helper.lazylib.Watch, 'lazylib.Watch');
+        assert.isDefined(helper.lazylib.Watcher, 'lazylib.Watcher');
     });
 });
 
